1d4c281a8e4ad61fdd0afebb25d8dab73ffab0e0
Ryan Monaghan <monaghanryan95@gmail.com>
2019-06-22 18:51:38 +0100

    Update md5.sh

--- md5.sh
+++ md5.sh
@@ -23,18 +23,10 @@ VTHash=$(md5sum $(ls -t | head -n1) | awk '{print $1}') #store md5hash of most r
 #echo Hash to be scanned: $VTHash
 printf "${GRN}VirusTotal Summary:${NC}"
 #Post hash to VT
-curl -s -X POST 'https://www.virustotal.com/vtapi/v2/file/report' --form apikey="63c74add1fbcb18cd03e860185b2716cc6f756071f698674d7ab125d5e38840d" --form resource=$VTHash | awk -F 'positives\":' '{printf "\nVT Hits: " $2}' | awk -F ' ' '{print $1$2$3$6$7}' | sed 's|["}]||g' 
+curl -s -X POST 'https://www.virustotal.com/vtapi/v2/file/report' --form apikey="YOUR_API_TOKEN_HERE" --form resource=$VTHash | awk -F 'positives\":' '{printf "\nVT Hits: " $2}' | awk -F ' ' '{print $1$2$3$6$7}' | sed 's|["}]||g' 
 echo '================================'
 read -n 1 -s -r -p "Press any key to generate full VirusTotal Report..."
 echo""
 printf "${GRN}Full VirusTotal Report:${NC}"
-curl -s -X POST 'https://www.virustotal.com/vtapi/v2/file/report' --form apikey="63c74add1fbcb18cd03e860185b2716cc6f756071f698674d7ab125d5e38840d" --form resource=$VTHash | sed 's|\},|\}\n|g'
+curl -s -X POST 'https://www.virustotal.com/vtapi/v2/file/report' --form apikey="YOUR_API_TOKEN_HERE" --form resource=$VTHash | sed 's|\},|\}\n|g'
 echo ""
-
-#| awk '{print $644,$645}'
-#| awk -F 'positives\":' '{print "VT Hits" $2}' | awk -F ' ' '{print $1$2$3$6$7}' | sed 's|["}]||g'
-#| sed 's|\},|\}\n|g'
-#| sed 's/\t/,|,/g' | column -s ',' -t
-#API Token: 63c74add1fbcb18cd03e860185b2716cc6f756071f698674d7ab125d5e38840d
-#VT Test:
-#md5sum $(ls -t | head -n1) | awk '{print $1}'
