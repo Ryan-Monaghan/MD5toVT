d3e4a3a2614c3c3cd04c0120459f02e4cf7589e5
Ryan Monaghan <monaghanryan95@gmail.com>
2019-06-22 18:52:21 +0100

    Update md5.sh

--- md5.sh
+++ md5.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/env bash
-RED='\033[0;31m' #store literal color value for red
-GRN='\033[0;32m' #store literal color value for green
-NC='\033[0m' # no color
-bold=$(tput bold) #variable to format text as bold
-normal=$(tput sgr0) #variable to format text as normal
-
-cd /home/$USER/Downloads/ #change to current users downloads folder
-mostRecent=$(ls -t | head -n1) #store the most recently downloaded file as variable
-echo '================================'
+RED='\033[0;31m'
+GRN='\033[0;32m'
+NC='\033[0m'
+bold=$(tput bold)
+normal=$(tput sgr0)
+cd /home/$USER/Downloads/
+mostRecent=$(ls -t | head -n1)
+echo '--------------------------------'
 printf "${GRN}Downloads Folder Hash/Filename\n${NC}"
-echo '================================'
-for file in *; do #for each file in d/l folder
-	md5sum "$file" #extract the md5hash
-done | sed 's/  /+,/' | awk -F"+" '{print $1, $2, $3}' | sed 's/ ,/,/g' #formatting make output csv friendly
-
-echo '================================'
+echo '--------------------------------'
+for file in *; do
+	md5sum "$file"
+done | sed 's/  /+,/' | awk -F"+" '{print $1, $2, $3}' | sed 's/ ,/,/g'
+echo '--------------------------------'
 printf "${GRN}Most Recent File: ${NC}"
-md5sum $mostRecent #extract md5sum of most recent d/l file
-
-echo '================================'
-VTHash=$(md5sum $(ls -t | head -n1) | awk '{print $1}') #store md5hash of most recently d/l file as variable
-#echo Hash to be scanned: $VTHash
+md5sum $mostRecent
+echo '--------------------------------'
+VTHash=$(md5sum $(ls -t | head -n1) | awk '{print $1}')
 printf "${GRN}VirusTotal Summary:${NC}"
-#Post hash to VT
 curl -s -X POST 'https://www.virustotal.com/vtapi/v2/file/report' --form apikey="YOUR_API_TOKEN_HERE" --form resource=$VTHash | awk -F 'positives\":' '{printf "\nVT Hits: " $2}' | awk -F ' ' '{print $1$2$3$6$7}' | sed 's|["}]||g' 
-echo '================================'
+echo '--------------------------------'
 read -n 1 -s -r -p "Press any key to generate full VirusTotal Report..."
-echo""
+printf "\n"
 printf "${GRN}Full VirusTotal Report:${NC}"
 curl -s -X POST 'https://www.virustotal.com/vtapi/v2/file/report' --form apikey="YOUR_API_TOKEN_HERE" --form resource=$VTHash | sed 's|\},|\}\n|g'
-echo ""
+printf "\n"
+
+#| awk '{print $644,$645}'
+#| awk -F 'positives\":' '{print "VT Hits" $2}' | awk -F ' ' '{print $1$2$3$6$7}' | sed 's|["}]||g'
+#| sed 's|\},|\}\n|g'
+#| sed 's/\t/,|,/g' | column -s ',' -t
+#API Token: 63c74add1fbcb18cd03e860185b2716cc6f756071f698674d7ab125d5e38840d
+#VT Test:
+#md5sum $(ls -t | head -n1) | awk '{print $1}'
